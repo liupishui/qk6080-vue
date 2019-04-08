8504cf8d5a654cee1bb949ea7c69cf41f8693eda
zhuifengh2o <liupishui@163.com>
2019-04-08 19:35:19 +0800

    1

--- src/common/api/extend/test.js
+++ src/common/api/extend/test.js
@@ -1,6 +1,6 @@
-function api(params) {
-    console.log('---------', this.sliders.getAllSliders().then(rst => {
-        console.log(rst)
-    }))
+function api (params) {
+  console.log('---------', this.sliders.getAllSliders().then(rst => {
+    console.log(rst)
+  }))
 }
 export default api
