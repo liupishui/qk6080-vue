f1cf1ab55fc6c84b0de77e11aceeed2d66954e77
liupishui <liupishui@163.com>
2019-11-07 15:41:22 +0800

    啊

--- src/router/_import_production.js
+++ src/router/_import_production.js
@@ -1,1 +1,3 @@
-module.exports = file => require('@/pages/' + file + '.vue').default // vue-loader at least v13.0.0+
+// module.exports = file => require('@/pages/' + file + '.vue').default // vue-loader at least v13.0.0+
+
+module.exports = file => () => import('@/pages/' + file + '.vue')
